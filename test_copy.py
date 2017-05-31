488fc70fa42c5b10b892491770e77399edc8aabb
Yuan Jiang <jiangyuan1988@gmail.com>
2017-05-31 15:15:19 +0800

    shallow and deep copy

---
+++ test_copy.py
@@ -0,0 +1,27 @@
+import copy
+# print(copy.__file__)
+
+a = [1, 2, 3]
+b = [4, 5, 6]
+c = [a, b]
+
+# using normal assignment operations to copy:
+d = c
+# True - d is the same object as c
+print("normal assign:id(c)==id(d):{},id(c)=={},id(d)={}".format(id(c)==id(d), id(c), id(d)))
+# True - d[0] is the same object as c[0]
+print("normal assign:id(c[0])==id(d[0]):{},id(c[0])=={},id(d[0])={}".format(id(c[0])==id(d[0]), id(c[0]), id(d[0])))
+
+# using a shallow copy
+d = copy.copy(c)
+# False - d is now a new object
+print("shallow copy:id(c)==id(d):{},id(c)={},id(d)={}".format(id(c)==id(d),id(c),id(d)))
+# True - d[0] is the same object as c[0]
+print("shallow copy:id(c[0]) == id(d[0]):{},id(c[0])={},id(d[0])={}".format(id(c[0])==id(d[0]), id(c[0]), id(d[0])))
+
+# using a deep copy
+d = copy.deepcopy(c)
+# False - d is now a new object
+print("deep copy:id(c)==id(d):{},id(c)={},id(d)={}".format(id(c)==id(d),id(c),id(d)))
+# False - d[0] is a new object
+print("deep copy:id(c[0]) == id(d[0]):{},id(c[0])={},id(d[0])={}".format(id(c[0])==id(d[0]), id(c[0]), id(d[0])))
