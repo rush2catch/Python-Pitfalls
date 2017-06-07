a21759da836b1c78742d549383448aad6eabecde
Yuan Jiang <jiangyuan1988@gmail.com>
2017-06-07 18:50:26 +0800

    time consumption calculation in python

--- time_consuming.py
+++ time_consuming.py
@@ -11,6 +11,7 @@ end_time = datetime.datetime.now()
 print("{}s".format(end_time - start_time))
 
 # method 2 - time.time()
+# to calculate in seconds
 import time
 start = time.time()
 a = []
@@ -21,6 +22,7 @@ end = time.time()
 print("{}s".format(end - start))
 
 # method 3 - time.clock()
+# to calculate in system clock times
 import time
 start = time.clock()
 a = []
