40154eab882674cee4f2c677f583eb48d7d96e0f
Yuan Jiang <jiangyuan1988@gmail.com>
2017-06-07 18:48:42 +0800

    time consumption calculation in python

---
+++ time_consuming.py
@@ -0,0 +1,31 @@
+# 3 main ways to calculate the time consumption in python
+
+# method 1 - datetime
+import datetime
+start_time = datetime.datetime.now()
+a = []
+for i in range(10000000, 0, -1):
+    a.append(i)
+a.sort()
+end_time = datetime.datetime.now()
+print("{}s".format(end_time - start_time))
+
+# method 2 - time.time()
+import time
+start = time.time()
+a = []
+for i in range(10000000, 0, -1):
+    a.append(i)
+a.sort()
+end = time.time()
+print("{}s".format(end - start))
+
+# method 3 - time.clock()
+import time
+start = time.clock()
+a = []
+for i in range(10000000, 0, -1):
+    a.append(i)
+a.sort()
+end = time.clock()
+print("{} clock time".format(end - start))
