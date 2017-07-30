3854ef0d097eabcff0a392fdfbb9c1a939a0a181
rush2catch <jiangyuan1988@gmail.com>
2017-07-30 17:49:45 +0800

    classmethod and staticmethod in python

---
+++ classmethod.py
@@ -0,0 +1,17 @@
+class A(object):
+	def m1(self, n):
+		print("self:", self)
+
+	@classmethod
+	def m2(cls, n):
+		print("cls:", cls)
+
+	@staticmethod
+	def m3(n):
+		pass
+
+
+a = A()
+a.m1(1)
+A.m2(1)
+A.m3(1)
