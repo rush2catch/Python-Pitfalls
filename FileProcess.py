e321d5cb88022430a6c1393f60bee99925079649
Yuan Jiang <jiangyuan1988@gmail.com>
2017-09-22 18:52:04 +0800

    list intersection, union, and difference in python

--- FileProcess.py
+++ FileProcess.py
@@ -2,11 +2,14 @@
 
 class Solution(object):
 	def file_process(self):
+		count = 0
 		with open('fix.log', 'rb') as f:
 			for line in f.readlines():
 				arr = line.split()
 				if arr[-1] != arr[-2]:
+					count += 1
 					print(line)
+		print(count)
 
 obj = Solution()
 obj.file_process()
